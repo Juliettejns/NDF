3cbde3a638506686fd5af34ad5d6530397328cc3
Juliettejns <juliette.janes@yahoo.fr>
2021-02-20 18:32:41 +0100

    Suppression des extractions et insertion de données

--- app/donnees.py
+++ app/donnees.py
@@ -1,10 +1,8 @@
 """
-Initialisation de la base de données
-Extraction de données du fichier xml en vue de leur intégration dans la base
+Instanciation du modèle de base de données
 """
 
 from app import db
-from constantes import document_xml
 
 class Article(db.Model):
     #instanciation du modèle, définition de la classe
@@ -20,25 +18,3 @@ class Article(db.Model):
         self.date = date
         self.texte = texte
 
-db.drop_all()
-db.create_all()
-
-
-liste_titre = []
-liste_date = []
-liste_texte = []
-
-namespaces = {'tei': 'http://www.tei-c.org/ns/1.0'}
-for element in document_xml.xpath("//tei:group/tei:text", namespaces=namespaces):
-    numero = element.attrib['n']
-    element_titre = document_xml.xpath("//tei:docTitle/tei:titlePart[@type='sub']/text()", namespaces=namespaces)
-    liste_titre.append(element_titre)
-    element_date = document_xml.xpath("//tei:docDate/tei:date/text()", namespaces=namespaces)
-    liste_date.append(element_date)
-    element_texte = document_xml.xpath("//tei:body/tei:div/tei:p/text()", namespaces=namespaces)
-    liste_texte.append(element_texte)
-    db.session.add(Article(numero,
-                           liste_titre[int(numero) - 1],
-                            liste_date[int(numero) - 1],
-                           liste_texte[int(numero) - 1]))
-    db.session.commit()
