6464ae46979be3435134fbe8c616b644871b7fc5
Juliettejns <juliette.janes@yahoo.fr>
2021-03-02 18:36:21 +0100

    Séparation de données.py en extraction pour l'ajout des données extraites du xml et en instanciation de modele

--- app/extraction.py
+++ app/extraction.py
@@ -6,28 +6,17 @@ association de ces données à la bd
 from app import db
 from constantes import document_xml
 from donnees import Article
-
-#Réinitalisation de la base à chaque lancement
 db.drop_all()
 db.create_all()
 
-liste_titre=[]
-liste_date=[]
-liste_texte=[]
-
 namespaces = {'tei': 'http://www.tei-c.org/ns/1.0'}
-
-for element in document_xml.xpath("//tei:group/tei:text", namespaces=namespaces):
-    numero = element.attrib['n']
-    element_titre = document_xml.xpath("//tei:docTitle/tei:titlePart[@type='sub']/text()", namespaces=namespaces)
-    liste_titre.append(element_titre)
+for numero in document_xml.xpath("//tei:group/tei:text/@n", namespaces=namespaces):
+    element_titre = document_xml.xpath("//tei:titlePart[@type='sub']/text()", namespaces=namespaces)
     element_date = document_xml.xpath("//tei:docDate/tei:date/text()", namespaces=namespaces)
-    liste_date.append(element_date)
     element_texte = document_xml.xpath("//tei:body/tei:div/tei:p/text()", namespaces=namespaces)
-    liste_texte.append(element_texte)
-    db.session.add(Article(numero,
-                           liste_titre[int(numero)-1],
-                           liste_date[int(numero)-1],
-                           liste_texte[int(numero)-1]))
+    note=Article(article_titre=element_titre[int(numero)-1], article_date=element_date[int(numero)-1], article_texte=element_texte[int(numero)-1])
+    db.session.add(note)
     db.session.commit()
 
+article=Article.query.all()
+print(Article.query.filter_by(article_titre="Heures d'angoisse").first())
