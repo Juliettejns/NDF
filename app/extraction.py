6a1cc361fde6df74bd017f94965ccce13c5a9532
Juliettejns <juliette.janes@yahoo.fr>
2021-03-03 17:29:09 +0100

    Extraction des noms et prénoms des personnes et association à la db

--- app/extraction.py
+++ app/extraction.py
@@ -5,14 +5,33 @@ association de ces données à la bd
 
 from app import db
 from constantes import document_xml
-from modeles.donnees import Article
+from modeles.donnees import *
 db.drop_all()
 db.create_all()
 
 namespaces = {'tei': 'http://www.tei-c.org/ns/1.0'}
-for numero in document_xml.xpath("//tei:group/tei:text/@n", namespaces=namespaces):
-    element_titre = document_xml.xpath("//tei:titlePart[@type='sub']/text()", namespaces=namespaces)
-    element_date = document_xml.xpath("//tei:docDate/tei:date/text()", namespaces=namespaces)
-    note=Article(article_titre=element_titre[int(numero)-1], article_date=element_date[int(numero)-1])
+
+n=0
+element_titre = document_xml.xpath("//tei:titlePart[@type='sub']/text()", namespaces=namespaces)
+element_date = document_xml.xpath("//tei:docDate/tei:date/@when-iso", namespaces=namespaces)
+element_numeroJournal = document_xml.xpath("//tei:text/@n", namespaces=namespaces)
+for element in document_xml.xpath("//tei:group/tei:text", namespaces=namespaces):
+    n+=1
+    note=Article(article_titre=element_titre[int(n)-1],
+                 article_date=element_date[int(n)-1],
+                 article_numJournal=element_numeroJournal[int(n)-1]
+                 )
     db.session.add(note)
+    db.session.commit()
+
+element_nom = document_xml.xpath("//tei:persName/tei:surname/text()", namespaces=namespaces)
+element_prenom = document_xml.xpath("//tei:persName/tei:forename/text()", namespaces=namespaces)
+element_note = document_xml.xpath("//tei:person/tei:note/text()", namespaces=namespaces)
+n=0
+for element in document_xml.xpath("//tei:listPerson/tei:person", namespaces=namespaces):
+    n+=1
+    personne = Personne(personne_nom = element_nom[int(n)-1],
+                        personne_prenom = element_prenom[int(n)-1],
+                        personne_notes = element_prenom[int(n)-1])
+    db.session.add(personne)
     db.session.commit()
