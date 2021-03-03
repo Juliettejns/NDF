4accf0e7f0798c33e28c637cb183a6e3023fe106
Juliettejns <juliette.janes@yahoo.fr>
2021-03-03 18:44:05 +0100

    Ajout de role dreyf et numérotation des person sur le xml pour ce faire

--- app/extraction.py
+++ app/extraction.py
@@ -26,20 +26,26 @@ for element in document_xml.xpath("//tei:group/tei:text", namespaces=namespaces)
 
 element_nom_personne = document_xml.xpath("//tei:persName/tei:surname/text()", namespaces=namespaces)
 element_prenom = document_xml.xpath("//tei:persName/tei:forename/text()", namespaces=namespaces)
-element_note = document_xml.xpath("//tei:person/tei:note/text()", namespaces=namespaces)
+element_dreyf=[]
 n=0
 for element in document_xml.xpath("//tei:listPerson/tei:person", namespaces=namespaces):
     n+=1
+    if document_xml.xpath("//tei:person[@n=" + str(n) + "and @role='dreyf']", namespaces=namespaces):
+        element_dreyf.append('True')
+    elif document_xml.xpath("//tei:person[@n=" + str(n) + "and @role='antidreyf']", namespaces=namespaces):
+        element_dreyf.append('False')
+    else:
+         element_dreyf.append('')
+
     personne = Personne(personne_nom = element_nom_personne[int(n)-1],
                         personne_prenom = element_prenom[int(n)-1],
-                        personne_notes = element_prenom[int(n)-1]
+                        personne_role_dreyf = element_dreyf[int(n)-1],
                         )
     db.session.add(personne)
     db.session.commit()
 
 element_nom_lieu = document_xml.xpath("//tei:place/tei:placeName/text()", namespaces=namespaces)
 n=0
-print(element_nom_lieu)
 for element in document_xml.xpath("//tei:listPlace/tei:place", namespaces= namespaces):
     n+=1
     lieu = Lieu(lieu_nom = element_nom_lieu[int(n)-1]
--- app/modeles/donnees.py
+++ app/modeles/donnees.py
@@ -9,6 +9,35 @@ class Article(db.Model):
     __table_args__ = {'extend_existing': True}
     #instanciation du modèle, définition de la classe
     article_id = db.Column(db.Integer, unique=True, nullable=False, primary_key=True, autoincrement=True)
-    article_titre = db.Column(db.String(45))
-    article_date = db.Column(db.String(45))
+    article_titre = db.Column(db.String(45), nullable=False)
+    article_date = db.Column(db.String(45), nullable=False)
+    article_numJournal = db.Column(db.Integer, nullable=False)
 
+class Personne(db.Model):
+    __tablename__="personne"
+    __table_args__ = {'extend_existing': True}
+    personne_id = db.Column(db.Integer, unique=True, nullable=False, primary_key=True, autoincrement=True)
+    personne_nom = db.Column(db.String(45), nullable=False)
+    personne_prenom = db.Column(db.String(45), nullable=False)
+    personne_date_naissance = db.Column(db.String(45))
+    personne_date_mort = db.Column(db.String(45))
+    personne_role_dreyf = db.Column(db.String(45))
+    personne_notes = db.Column(db.Text)
+
+class Lieu(db.Model):
+    __tablename__ = "lieu"
+    __table_args__ = {'extend_existing': True}
+    lieu_id = db.Column(db.Integer, unique=True, nullable=False, primary_key=True, autoincrement=True)
+    lieu_nom = db.Column(db.String(45), nullable=False)
+    lieu_emplacement = db.Column(db.String(63))
+    lieu_notes = db.Column(db.Text)
+
+
+articleHasPersonne = db.Table('articleHasPersonne',
+                           db.Column('artid', db.Integer, db.ForeignKey('article.article_id')),
+                           db.Column('persid', db.Integer, db.ForeignKey('personne.personne_id'))
+                            )
+articleHasLieu = db.Table('articleHasLieu',
+                          db.Column('artid', db.Integer, db.ForeignKey('article.article_id')),
+                          db.Column('lid', db.Integer, db.ForeignKey('lieu.lieu_id'))
+                          )
