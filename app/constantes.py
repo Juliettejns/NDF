e6e0b1e177f9f679aa83930320fd6cefd7a4286e
Juliettejns <juliette.janes@yahoo.fr>
2021-03-09 16:31:29 +0100

    Parsage et lecture comme XSLT de la feuille de transformation pour l'affichage du texte des articles

--- app/constantes.py
+++ app/constantes.py
@@ -6,3 +6,6 @@ from lxml import etree
 
 
 document_xml = etree.parse("xml_tei/NDF_TEI.xml")
+
+affichage_texte = etree.parse("xml_tei/xslt_affichage_note.xsl")
+xslt_transformation = etree.XSLT(affichage_texte)
