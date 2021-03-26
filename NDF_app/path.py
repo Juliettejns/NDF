899ae0680b62abfa748a32440c1aea4adbd87a8b
Juliette Janes <juliette.janes@yahoo.fr>
2021-03-26 18:50:17 +0100

    Déplacement ndf_app

--- NDF_app/path.py
+++
@@ -1,8 +0,0 @@
-import os
-
-# définition des chemins
-chemin_actuel = os.path.dirname(os.path.abspath(__file__))
-print(chemin_actuel)
-
-templates = os.path.join(chemin_actuel, "app", "templates")
-print(templates)
